c52f318f47ee1e66a130b630e29880649366a01a
Luukas Pörtfors <lajp@iki.fi>
2024-02-25 20:07:03 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          packages = [ pkgs.cargo pkgs.rustc ];
+        };
+      };
+    };
+}
