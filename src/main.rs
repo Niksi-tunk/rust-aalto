6792fbb783bc53995155b1bec06d9648a795f42f
Luukas Pörtfors <lajp@iki.fi>
2024-03-12 20:17:59 +0200

    feat: make rust course

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Exercise 1:
+// Below you see the main function of this Rust program
+// The main function is always the first function that is
+// executed on program startup.
+//
+// The current main function prints out "Hello, world!"
+//
+// Your task is to:
+// Change the program to print out "Hello, demo!"
+
 fn main() {
     println!("Hello, world!");
 }
